a966fe6ee2b3a5939e8d142952909a66cd0b0464
窪田 光 <h-kubota@njcadmin.local>
2016-04-15 16:22:57 +0900

    Add chapter09

---
+++ 9/constant_service.js
@@ -0,0 +1,10 @@
+app.constant('ConstMessage', 'This is my constant!');
+
+app.constant('ConstValue', {
+    value1: 12345,
+    value2: 67890
+});
+
+app.constant('ConstFunc', function(a, b){
+    return a * b;
+});
---
+++ 9/value_service.js
@@ -0,0 +1,10 @@
+app.value('MyMessage', 'This is my value.');
+
+app.value('MyValue', {
+    value1: 12345,
+    value2: 12345
+});
+
+app.value('MyFunc', function(a, b){
+    return a + b;
+});

3f8984d74e3fe4b898baf29f38cea046a59bc88c
窪田 光 <h-kubota@njcadmin.local>
2016-04-18 11:44:14 +0900

    Add カスタムディレクティブ index.html

--- chapter9/instanceable_controller.js
+++ chapter9/instanceable_controller.js
@@ -2,6 +2,7 @@ app.controller('TaxController', ['$scope', 'TaxCalculatorFactory',
     function($scope, TaxCalculatorFactory) {
         var taxCalculator5 = TaxCalculatorFactory(0.05);
         var taxCalculator8 = TaxCalculatorFactory(0.08);
+        var taxCalculator10 = TaxCalculatorFactory(0.10);
 
         $scope.calculate5 = function(price) {
             return taxCalculator5.calculate(price);
@@ -9,6 +10,10 @@ app.controller('TaxController', ['$scope', 'TaxCalculatorFactory',
 
         $scope.calculate8 = function(price) {
             return taxCalculator8.calculate(price);
+        }
+
+        $scope.calculate10 = function(price) {
+            return taxCalculator10.calculate(price);
         }
 
     }
